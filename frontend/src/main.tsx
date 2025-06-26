1573013e301c7316fa7df16e3ca595049143e933
Nirmal Jyoti Biswas <nirmaljyotib@gmail.com>
2025-06-26 12:48:22 +0530

    added a sample qr code, soon gonna have individual for each student

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,6 +14,7 @@ import Dashboard from './pages/Dashboard.tsx'
 import StudentMainLayout from './StudentMainLayout.tsx'
 import Queries from './pages/Queries.tsx'
 import Scanner from './pages/admin/Scanner.tsx'
+import MyQrcode from './pages/MyQrcode.tsx'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -34,6 +35,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/student/app" element={<StudentMainLayout /> }>
             <Route index element={<Dashboard />}/>
             <Route path="queries-complaints" element={<Queries />}/>
+            <Route path="qrcode" element={<MyQrcode />}/>
           </Route>
           <Route path="/admin/app">
             <Route path="scanner" element={<Scanner />} />
