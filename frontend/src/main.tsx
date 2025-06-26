26ff8950cb9367b8c8dafd1279f2824e93ce5fb3
Nirmal Jyoti Biswas <nirmaljyotib@gmail.com>
2025-06-26 14:26:46 +0530

    added stuff

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -15,6 +15,7 @@ import StudentMainLayout from './StudentMainLayout.tsx'
 import Queries from './pages/Queries.tsx'
 import Scanner from './pages/admin/Scanner.tsx'
 import MyQrcode from './pages/MyQrcode.tsx'
+import Payment from './pages/Payment.tsx'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -34,6 +35,7 @@ createRoot(document.getElementById('root')!).render(
           </Route>
           <Route path="/student/app" element={<StudentMainLayout /> }>
             <Route index element={<Dashboard />}/>
+            <Route path="payment" element={<Payment />}/>
             <Route path="queries-complaints" element={<Queries />}/>
             <Route path="qrcode" element={<MyQrcode />}/>
           </Route>
