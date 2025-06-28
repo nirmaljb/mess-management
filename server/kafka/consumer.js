a4f35606f05d8736343f031f806af1fc332f2658
Nirmal Jyoti Biswas <nirmaljyotib@gmail.com>
2025-06-29 00:46:43 +0530

    fixed some code

--- server/kafka/consumer.js
+++ server/kafka/consumer.js
@@ -1,12 +1,24 @@
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['localhost:9092', 'localhost:9092'],
+});
+
+
 const consumer = kafka.consumer({ groupId: 'test-group' })
 
-await consumer.connect()
-await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
-
-await consumer.run({
-  eachMessage: async ({ topic, partition, message }) => {
-    console.log({
-      value: message.value.toString(),
+const main = async() => {
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+    
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log({
+                value: message.value.toString(),
+            })
+        },
     })
-  },
-})
+};
+
+main();
--- server/kafka/producer.js
+++ server/kafka/producer.js
@@ -2,17 +2,21 @@ const { Kafka } = require('kafkajs')
 
 const kafka = new Kafka({
   clientId: 'my-app',
-  brokers: ['kafka1:9092', 'kafka2:9092'],
+  brokers: ['localhost:9092', 'localhost:9092'],
 })
 
 const producer = kafka.producer()
 
-await producer.connect()
-await producer.send({
-  topic: 'test-topic',
-  messages: [
-    { value: 'Hello KafkaJS user!' },
-  ],
-})
+const main = async() => {
 
-await producer.disconnect()
+    await producer.connect()
+    await producer.send({
+        topic: 'test-topic',
+        messages: [
+            { value: 'Hello KafkaJS user!' },
+        ],
+    });
+    // await producer.disconnect()
+};
+
+main();
