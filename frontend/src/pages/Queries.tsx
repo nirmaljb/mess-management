a76847c47b120763b756a61fa33c166bd554344a
Nirmal Jyoti Biswas <nirmaljyotib@gmail.com>
2025-06-30 23:36:58 +0530

    added code for frontend image upload

--- frontend/src/pages/Queries.tsx
+++ frontend/src/pages/Queries.tsx
@@ -14,10 +14,19 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
-import { useAuth } from "@clerk/clerk-react"
-import { redirect } from "react-router"
+import { Link } from "react-router"
+import { useState } from "react"
 
 const formSchema = z.object({
   subject: z.string().min(2, {
@@ -27,26 +36,29 @@ const formSchema = z.object({
     message: "Description must be at least 10 characters."
   }).max(160, {
     message: "Description must be less than 160 characters."
-  })
+  }),
+  category: z.string(),
+  image: z.string()
 })
 
 
 
 export default function Queries() {
 
-    // const { isSignedIn }= useAuth();
+  const [file, setFile] = useState(null);
 
-    // if(!isSignedIn) {
-    //     return redirect("/");
-    // }
-
-  // ...
+  function handleChange(e) {
+    console.log(e.target.files);
+    setFile(URL.createObjectURL(e.target.files[0]));
+  }
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       subject: "",
       description: "",
+      category: "",
+      image: ""
     },
   })
  
@@ -55,6 +67,7 @@ export default function Queries() {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
+    console.log(file);
     form.reset();
   }
 
@@ -72,6 +85,32 @@ export default function Queries() {
               </FormControl>
               <FormDescription>
                 This is your main subject.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="category"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Category</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a specific category" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="food_quality">Food Quality</SelectItem>
+                  <SelectItem value="staff">Staff</SelectItem>
+                  <SelectItem value="fee_payment">Fee Payment</SelectItem>
+                  <SelectItem value="others">Others</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormDescription>
+                Select a specific category of your query/complaint
               </FormDescription>
               <FormMessage />
             </FormItem>
@@ -97,19 +136,27 @@ export default function Queries() {
             </FormItem>
           )}
         />
-
-        <div className="flex items-center justify-center w-full">
-            <label title="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                    <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-                    </svg>
-                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                    <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                </div>
-                <input id="dropzone-file" type="file" className="hidden" />
-            </label>
-        </div> 
+        <FormField
+          control={form.control}
+          name="image"
+          render={({ field }) => (
+            <FormItem className="flex items-center justify-center w-full">
+              <label title="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                      <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                      </svg>
+                      <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                      <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                  </div>
+                  {/* <FormControl> */}
+                    <input id="dropzone-file" type="file" className="hidden" onChange={handleChange}/>
+                  {/* </FormControl> */}
+              {/* </FormLabel> */}
+              </label>
+          </FormItem> 
+          )}
+        />
 
         <Button type="submit">Submit</Button>
       </form>
